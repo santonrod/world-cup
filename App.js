15ff39dfbf159de1ea2c870cfd6ccc4a2faa5be3
Stefan <santonrod@gmail.com>
2019-08-05 23:44:21 -0500

    initialize project and scaffold folders

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+import Example from './app/components/Example'
+
+const App = () => {
+  return (
+    <Fragment>
+      <Example />
+    </Fragment>
+  );
+};
+
+export default App;
